e0c57aea14a931aa6babe4f2bb1b933162782842
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-10-12 08:34:41 +0300

    MY COMMIT

--- server/app.py
+++ server/app.py
@@ -17,10 +17,43 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict=[plant.to_dict() for plant in Plant.query.all()]
+        return make_response(response_dict,200)
+    
+    def post(self):
+        data=request.get_json()
+        
+        if not data :
+            response_body={
+                "error": "No data provided"
+            }
+            return make_response(response_body,400)
+        
+        name=data['name']
+        image=data['image']
+        price=data['price']
+            
+        new_plant=Plant(
+                name=name,
+                image=image,
+                price=price
+            )
+        db.session.add(new_plant)
+        db.session.commit()
+            
+        new_plant_dict=new_plant.to_dict()
+            
+        return make_response(new_plant_dict,201)
+            
+api.add_resource(Plants,'/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant_dict=Plant.query.filter_by(id=id).first().to_dict()
+        
+        return make_response(plant_dict,200)
+api.add_resource(PlantByID,'/plants/<int:id>')
         
 
 if __name__ == '__main__':
